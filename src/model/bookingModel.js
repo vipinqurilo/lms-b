cfecc15c3e22c11f5692cdbe90b21df19721281f
kdidrees <Khurshid@qurilo.com>
2025-02-19 16:45:22 +0530

    kd branch created

--- src/model/bookingModel.js
+++ src/model/bookingModel.js
@@ -1,26 +1,64 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const bookingSchema=new mongoose.Schema({
-    teacherId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
-    studentId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
-    subjectId:{type:mongoose.Schema.Types.ObjectId,ref:'Subcategory',required:true},
-    scheduledDate:{type:Date,required:true},
-    sessionStartTime:{type:Date,required:true},
-    sessionEndTime:{type:Date,required:true},
-    sessionDuration:{type:Number,required:true},
-    status:{type:String,required:true,enum: ["scheduled", "confirmed","reschedule_in_progress","rescheduled", "cancelled", "completed"], 
-        default: "scheduled"},
-    paymentId:{type:mongoose.Schema.Types.ObjectId,ref:'Payment',required:true},
-    meetingPlatform: { type: String, enum: ["Google Meet", "Zoom"], default: null },
+const bookingSchema = new mongoose.Schema(
+  {
+    teacherId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subjectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subcategory",
+      required: true,
+    },
+    scheduledDate: { type: Date, required: true },
+    sessionStartTime: { type: Date, required: true },
+    sessionEndTime: { type: Date, required: true },
+    sessionDuration: { type: Number, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: [
+        "scheduled",
+        "confirmed",
+        "reschedule_in_progress",
+        "rescheduled",
+        "cancelled",
+        "completed",
+      ],
+      default: "scheduled",
+    },
+    paymentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Payment",
+      required: true,
+    },
+    meetingPlatform: {
+      type: String,
+      enum: ["Google Meet", "Zoom"],
+      default: null,
+    },
     meetingLink: { type: String, default: null },
     meetingUsername: { type: String, default: null },
     meetingPassword: { type: String, default: null },
     rescheduleRequest: {
-        newTime: {type:String},
-        reason: {type:String},
-        status: { type: String, enum: ["pending", "accepted", "denied"], default: "pending" }
+      newTime: { type: String },
+      reason: { type: String },
+      status: {
+        type: String,
+        enum: ["pending", "accepted", "denied"],
+        default: "pending",
+      },
     },
-    cancellationReason:{type:String},
-    cancelledBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'}
-},{timestamps:true})
-module.exports=mongoose.model('Booking',bookingSchema);
+    cancellationReason: { type: String },
+    cancelledBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Booking", bookingSchema);
