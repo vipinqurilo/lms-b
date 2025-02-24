e8b628a47055a6c5093281e4860f3717f93be1be
kdidrees <Khurshid@qurilo.com>
2025-02-24 13:19:23 +0530

    added pagination in categories and sub categories api

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 const courseRouter = require("./src/route/courseRoutes");
 const authController = require("./src/route/authRoutes");
 const categoryRouter = require("./src/route/categoryRoute");
-const languageRouter=require("./src/route/languageRoute");
+const languageRouter = require("./src/route/languageRoute");
 const requestRouter = require("./src/route/requestRoutes");
 const profileRouter = require("./src/route/profileRoute");
 const tutorRouter = require("./src/route/tutorRoutes");
 const bookingRouter = require("./src/route/bookingRoute");
 const subcategoryRouter = require("./src/route/subCategoryRoute");
 const wishListRouter = require("./src/route/wishlistRoute");
-const orderRouter= require("./src/route/orderRoute");
+const orderRouter = require("./src/route/orderRoute");
 const reviewRoute = require("./src/route/reviewRoute");
 const ticketRouter = require("./src/route/ticketRoute");
 const userRoutes = require("./src/route/userRoutes");
@@ -24,41 +24,40 @@ const paymentRouter = require("./src/route/paymentRoutes");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-require('dotenv').config()
-const corsOption={
-    origin:"*",
-    optionsSuccessStatus:200
-}
+require("dotenv").config();
+const corsOption = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOption));
-app.use((req,res,next)=>{
-    // console.log(req)
-    next();
-})
+app.use((req, res, next) => {
+  // console.log(req)
+  next();
+});
 app.use("/public", express.static("public"));
 
-app.use("/api/requests",requestRouter)
-app.use("/api/auth",authController);
-app.use("/api/course",courseRouter);
-app.use("/api/category",categoryRouter);
-app.use("/api/languages",languageRouter);
-app.use("/api/bookings",bookingRouter);
-app.use("/api/profile",profileRouter)
-app.use("/api/tutors",tutorRouter);
-app.use("/api/subcategory",subcategoryRouter);
-app.use("/api/whishlist",wishListRouter);
-app.use('/api/order',orderRouter)
-app.use('/api/review',reviewRoute)
-app.use('/api/ticket',ticketRouter)
-app.use('/api/users',userRoutes)
- 
-app.use('/api/stripe',stripeRoute)
- 
-app.use('/api/wallet',walletRouter)
-app.use('/api/withdrawals',withdrawRouter)
-app.use('/api/students',studentRouter)
-app.use('/api/teachers',teacherRouter)
+app.use("/api/requests", requestRouter);
+app.use("/api/auth", authController);
+app.use("/api/course", courseRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/languages", languageRouter);
+app.use("/api/bookings", bookingRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/tutors", tutorRouter);
+app.use("/api/subcategory", subcategoryRouter);
+app.use("/api/whishlist", wishListRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/review", reviewRoute);
+app.use("/api/ticket", ticketRouter);
+app.use("/api/users", userRoutes);
 
+app.use("/api/stripe", stripeRoute);
+
+app.use("/api/wallet", walletRouter);
+app.use("/api/withdrawals", withdrawRouter);
+app.use("/api/students", studentRouter);
+app.use("/api/teachers", teacherRouter);
 
 //Payment Routes
-app.use("/api/payment",paymentRouter)
+app.use("/api/payment", paymentRouter);
 module.exports = app;
