e79a862cb3cfde6a765ebc576daf7cb6ba90c4c2
kdidrees <Khurshid@qurilo.com>
2025-03-08 12:49:24 +0530

    feat: add authMiddleware to teacher and request routes for enhanced security

--- src/route/teacherRoutes.js
+++ src/route/teacherRoutes.js
@@ -11,10 +11,10 @@ router.get("/",authMiddleware,getTeachers);
 
 //Teacher Requests
 router.post("/request",authMiddleware,authorizeRoles("admin,teacher"),createTeacherRequest);
-router.put("/request/:requestId",authorizeRoles("teacher"),editTeacherRequest);
-router.get("/request/:requestId",authorizeRoles("admin,teacher"),getTeacherRequestsById);
-router.put("/request/approve/:requestId",authorizeRoles("admin"),approvedTeacherRequest);
-router.put("/request/reject/:requestId",authorizeRoles("admin"),rejectedTeacherRequest);
-router.get("/request/all",authorizeRoles("admin"),getTeacherRequests);
+router.put("/request/:requestId",authMiddleware,authorizeRoles("teacher"),editTeacherRequest);
+router.get("/request/:requestId",authMiddleware,authorizeRoles("admin,teacher"),getTeacherRequestsById);
+router.put("/request/approve/:requestId",authMiddleware,authorizeRoles("admin"),approvedTeacherRequest);
+router.put("/request/reject/:requestId",authMiddleware, authorizeRoles("admin"),rejectedTeacherRequest);
+router.get("/request/all",authMiddleware, authorizeRoles("admin"),getTeacherRequests);
 const teacherRouter=router;
 module.exports=teacherRouter;
